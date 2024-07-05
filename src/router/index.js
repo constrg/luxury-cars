f1ae66bad2d84943f91c077835984d961b5e060c
Ricardo Garcia <ricardolacadgarcia@gmail.com>
2024-07-05 11:50:52 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from '../views/Home.vue'
 import CarList from '../views/CarList.vue'
 import CarInfo from '../views/CarInfo.vue'
+import NotFound from '../views/NotFound.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -19,6 +20,11 @@ const router = createRouter({
             path: '/car_info/:id',
             name: 'car_info',
             component: CarInfo
+        },
+        {
+            path: '/:catchAll(.*)*',
+            name: 'not_found',
+            component: NotFound
         }
     ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="not_found">
+        <div class="container">
+            <div class="not_found_content">
+                <h1 class="not_found_content_title">Not Found</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.not_found .not_found_content {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.not_found .not_found_content .not_found_content_title {
+    font-size: var(--fs-xl);
+    color: var(--clr-light);
+    text-align: center;
+}
+</style>
